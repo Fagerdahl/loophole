a5d7bfd531156d2db74873f1e455c87d08db1c54
Ronja Fagerdahl <ronjafagerdahl@gmail.com>
2025-11-25 22:23:53 +0100

    Feat: Add initial Spring Boot project setup

---
+++ loophole-service/src/main/java/com/fagerdahl/loophole/LoopholeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fagerdahl.loophole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoopholeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoopholeServiceApplication.class, args);
+    }
+
+}
---
+++ loophole-service/src/test/java/com/fagerdahl/loophole/LoopholeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fagerdahl.loophole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoopholeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
